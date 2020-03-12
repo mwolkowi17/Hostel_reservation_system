f92ac8c9395c0226eb427c4419ccf0d3c6f89fdc
Marcin Wołkowicz <mwolkowi17@hotmail.com>
2020-03-12 17:57:03 +0100

    data mode work on

---
+++ Pensjonat.Data/ReservationBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pensjonat.Data
+{
+    class ReservationBook
+    {
+        List<Room> ReservationOfRooms = new List<Room>();
+        List<Guest> GuestList = new List<Guest>();
+    }
+}
