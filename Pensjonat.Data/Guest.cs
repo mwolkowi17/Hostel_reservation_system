fea3778d69a258df7ee99095969387e9ee5deb70
Marcin Wołkowicz <mwolkowi17@hotmail.com>
2020-03-12 17:37:23 +0100

    Dodaj pliki projektów.

---
+++ Pensjonat.Data/Guest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pensjonat.Data
+{
+    class Guest
+    {
+        public int GuestID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Nationality { get; set; }
+        public int CreditCardNumber { get; set; }
+        public int NrofRoom { get; set; }
+
+        public Guest()
+        {
+
+        }
+
+        public Guest(string name, string surname, string nationality)
+        {
+            Name = name;
+            Surname = surname;
+            Nationality = nationality;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Surname + "Nr of the room" + NrofRoom;
+        }
+
+        public Queue<Room> reservationHistory = new Queue<Room>();
+    }
+}
---
+++ Pensjonat.Data/Room.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pensjonat.Data
+{
+    public enum RoomType
+       {
+           single,doublebed,threebed
+       }
+    public class Room
+    {
+        /*public enum RoomType
+        {
+            single,doublebed,threebed
+        }*/
+        public int RoomID { get; set; }
+        public int RoomNumber { get; set; }
+        public bool IfOccupied { get; set; }
+        public double Price { get; set; }
+        public RoomType Type { get; set; }
+        
+        public Room()
+        {
+
+        }
+
+        public Room(int roomnumber,double price,RoomType type)
+        {
+            RoomNumber = roomnumber;
+            Price = price;
+            Type = type;
+           
+        }
+        public override string ToString()
+        {
+            return "Nr pokoju "+RoomNumber + "- cena: " + Price;
+        }
+    }
+
+
+}
