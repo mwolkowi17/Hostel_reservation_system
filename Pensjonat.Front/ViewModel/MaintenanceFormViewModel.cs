40249521210f97602c8ddb997bf8743771d8a85b
Marcin Wołkowicz <mwolkowi17@hotmail.com>
2020-03-14 18:15:19 +0100

    interface going on

---
+++ Pensjonat.Front/ViewModel/MaintenanceFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pensjonat.Data;
+
+namespace Pensjonat.UI.ViewModel
+{
+   public class MaintenanceFormViewModel
+    {
+        public AdditionalMethods model = new AdditionalMethods();
+
+        public MaintenanceFormViewModel()
+        {
+
+        }
+
+    }
+}
